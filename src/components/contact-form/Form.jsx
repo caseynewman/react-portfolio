b2bf77249c18021204cace8c6a8bf4fff0e9666e
Yogi Spice <caseynnewman@yahoo.com>
2024-04-19 16:05:20 -0400

    created contact form

--- src/components/contact-form/Form.jsx
+++ src/components/contact-form/Form.jsx
@@ -1,27 +1,61 @@
 import { useState } from 'react'
 import { Card, CardBody } from "@chakra-ui/react"
-import Input from './Input'
 import Button from './Button'
+import { Input as ChakraInput, Stack, Textarea } from '@chakra-ui/react'
 
 const Form = () => {
-    const [input, setInput] = useState('');
-    const [isSuccess, setIsSuccess] = useState(false);
-    const [isError, setIsError] = useState(false)
 
-    const handleSubmit = () => {
-        fetch('/api/color', {
-            method: 'POST',
-            body: {
-                name: input
-            }
-        })
+    const [isSuccess, setIsSuccess] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [formValues, setFormValues] = useState({
+        name: '',
+        email: '',
+        message: '',
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert()
+        //check validity
     }
 
     return (
         <Card>
             <CardBody>
-                <Input value={input} setVal={setInput}/>
-                <Button onClick={handleSubmit}>Submit Form</Button>
+                <form onSubmit={handleSubmit}
+                    onChange={(event) => {
+                        console.log(formValues)
+                        const form = event.currentTarget;
+                        setFormValues({
+                            name: form.elements.name.value,
+                            email: form.elements.email.value,
+                            message: form.elements.message.value
+                        })
+                        console.log(formValues)
+                    }}
+                    >
+                    <Stack spacing={2}>
+                        <label htmlFor='name'>Name</label>
+                        <ChakraInput
+                            id='name'
+                            placeholder='Enter your name'
+                            defaultValue={formValues.name}
+                        />
+                        <label htmlFor='email'>Email</label>
+                        <ChakraInput
+                            id='email'
+                            placeholder='Enter your email address'
+                            defaultValue={formValues.email}
+                        />
+                        <label htmlFor='message'>Message</label>
+                        <Textarea
+                            id='message'
+                            placeholder='Enter your message here'
+                            defaultValue={formValues.message}
+                        />
+                    </Stack>
+                    <Button type='submit'>Submit Form</Button>
+                </form>
             </CardBody>
         </Card>
 
