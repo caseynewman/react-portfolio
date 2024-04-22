6db7e0b8a697bd03ed033ea58cdbc0d2bc516e29
Yogi Spice <caseynnewman@yahoo.com>
2024-04-22 09:04:47 -0400

    added email and phone  number to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,10 +2,15 @@ import Form from '../components/Form'
 
 export default function Contact() {
     return (
-      <div>
-        <h1>Contact Me</h1>
+        <div>
+            <h1>Contact Me</h1>
+            <div className='contact-info'>
+                <p>
+                    <b>email:</b> casey.n.newman@gmail.com<br />
+                    <b>phone:</b> 919.272.1817
+                </p>
+            </div>
             <Form />
-      </div>
+        </div>
     );
-  }
-  
+}
