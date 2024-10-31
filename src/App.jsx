d5b5cb617833bf967ae09c140f459aaf2dba078e
Yogi Spice <caseynnewman@yahoo.com>
2024-10-31 07:39:41 -0400

    changed background graident to move with mouse

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,31 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import './App.css'
 import Footer from './components/Footer';
+import { useEffect, useRef } from 'react';
+import { background } from '@chakra-ui/react';
+import { color } from 'framer-motion';
 
 function App() {
+  const bgRef = useRef(null);
+
+  useEffect(() => {
+    const updateMousePosition = (event) => {
+      if (!bgRef.current) return;
+      const { clientX, clientY } = event;
+      bgRef.current.style.setProperty('--x', `${clientX}px`);
+      bgRef.current.style.setProperty('--y', `${clientY}px`);
+    }
+
+    window.addEventListener('mousemove', updateMousePosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition);
+    };
+  }, [])
+
   return (
     <>
+    <div ref={bgRef} className='gradient'>
       <header>
         <Header />
       </header>
@@ -15,6 +36,7 @@ function App() {
       <footer>
         <Footer />
       </footer>
+    </div>
     </>
   )
 }
