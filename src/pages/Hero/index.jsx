305210bbf37ef3fdc8bed24896b5d6a792838d66
Yogi Spice <caseynnewman@yahoo.com>
2024-11-03 21:15:37 -0500

    adjusting section margins and navbar

--- src/pages/Hero/index.jsx
+++ src/pages/Hero/index.jsx
@@ -3,7 +3,7 @@ import './style.css';
 export default function Hero() {
     return (
         <section className='hero'>
-            <h1>Hi there, I'm Casey</h1>
+            <h1>Casey Newman</h1>
             <h2>Web Developer + Designer</h2>
         </section>
     )
