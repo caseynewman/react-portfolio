6e779b77d0d8cb3532b9739be8a2471d1a9d0723
Yogi Spice <caseynnewman@yahoo.com>
2024-11-05 14:50:44 -0500

    adjusted section padding and removed resume from Home

--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -3,7 +3,7 @@ import './style.css';
 export default function AboutSection () {
     return (
         <section className="about">
-        <h1>Hey, I'm Casey!</h1>
+        <h2>Hey, I'm Casey!</h2>
         </section>
     )
 }
