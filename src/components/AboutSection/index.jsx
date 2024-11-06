66276c2c6f20fcdc0d086712db6fb8084023403c
Yogi Spice <caseynnewman@yahoo.com>
2024-11-06 08:43:35 -0500

    added background color to Hero, added image to about section

--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -1,10 +1,20 @@
 import './style.css';
+import { Center, Image, TagRightIcon } from '@chakra-ui/react';
 
 export default function AboutSection() {
     return (
         <section className="about">
+            <div className='casey-headshot'>
+            <Image
+                    // borderRadius='full'
+                    boxSize='250px'
+                    padding={5}
+                    src='/assets/casey-newman.jpg'
+                    alt='Casey Newman'
+                />
+            </div>
             <h2><span className='underline'>Hey there,</span><br />
-             I'm Casey!</h2>
+                I'm Casey!</h2>
             <article className='about-section-info'>
                 <p>
                     I'm a web developer, writer, and yoga teacher based in Wilmington, NC. I have a BA in Communication Studies + a Master of Letters in Creative Writing from the University of Stirling in Scotland. I also have a technical certification in Full Stack Web Development from UNC Chapel Hill.
