3960d69387001ba2424baf6d011a864c77ee8098
Yogi Spice <caseynnewman@yahoo.com>
2024-11-05 22:00:00 -0500

    adjusting styling

--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -1,9 +1,17 @@
 import './style.css';
 
-export default function AboutSection () {
+export default function AboutSection() {
     return (
         <section className="about">
-        <h2>Hey, I'm Casey!</h2>
+            <h2><span className='underline'>Hey there,</span><br />
+             I'm Casey!</h2>
+            <article className='about-section-info'>
+                <p>
+                    I'm a web developer, writer, and yoga teacher based in Wilmington, NC. I have a BA in Communication Studies + a Master of Letters in Creative Writing from the University of Stirling in Scotland. I also have a technical certification in Full Stack Web Development from UNC Chapel Hill.
+                    <br /><br />
+                    With a background in both wellness and professional writing, I strive for a holistic approach to brand growth and sustainability through development, content, and design.
+                </p>
+            </article>
         </section>
     )
 }
