c682ca3065c37fdf0c6b8014e97cf74cff9eb899
Yogi Spice <caseynnewman@yahoo.com>
2024-11-08 14:58:52 -0500

    added images to featured section

--- src/components/FeaturedSection/index.jsx
+++ src/components/FeaturedSection/index.jsx
@@ -6,6 +6,14 @@ export default function FeaturedWork() {
             <div className='featured-header'>
                 <h2><span className='underline'>Full-Stack + Frontend Applications</span><br />Featured Work</h2>
             </div>
+            <article className='featured-work'>
+                <img src='/assets/bubble-app.png'
+                alt='Bubble App'></img>
+                <img src='/assets/quiet-soul-homepage.png'
+                alt='Quiet Soul App'></img>
+                <img src='/assets/weather-dashboard-screenshot.png'
+                alt='Weather Dashboard'></img>
+            </article>
         </section>
     )
 }
