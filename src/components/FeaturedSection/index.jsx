f348d4a1ca5922aa02ddb6f0ed5d9bd23303373a
Yogi Spice <caseynnewman@yahoo.com>
2024-11-09 20:38:01 -0500

    working on Featured section alignment

--- src/components/FeaturedSection/index.jsx
+++ src/components/FeaturedSection/index.jsx
@@ -8,12 +8,14 @@ export default function FeaturedWork() {
                 <h2><span className='underline'>Full-Stack + Frontend Applications</span><br />Featured Work</h2>
             </div>
             <article className='featured-work'>
-                <img src='/assets/bubble-app.png'
-                    alt='Bubble App'></img>
-                <img src='/assets/quiet-soul-homepage.png'
-                    alt='Quiet Soul App'></img>
-                <img src='/assets/weather-dashboard-screenshot.png'
-                    alt='Weather Dashboard'></img>
+                <div className='image-container'>
+                    <img src='/assets/bubble-app.png'
+                        alt='Bubble App'></img>
+                    <img src='/assets/quiet-soul-homepage.png'
+                        alt='Quiet Soul App'></img>
+                    <img src='/assets/weather-dashboard-screenshot.png'
+                        alt='Weather Dashboard'></img>
+                </div>
                 <MyButton>
                     <a href='/portfolio' target='_blank'>View Portfolio</a>
                 </MyButton>
