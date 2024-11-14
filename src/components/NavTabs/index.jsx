0342549377e1e94cef8051053cdf931fa4ab6ec8
Yogi Spice <caseynnewman@yahoo.com>
2024-11-14 15:09:07 -0500

    added toggleMenu function using useState

--- src/components/NavTabs/index.jsx
+++ src/components/NavTabs/index.jsx
@@ -1,44 +1,38 @@
 import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 import './style.css'
 
 function NavTabs() {
   const currentPage = useLocation().pathname;
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((open) => !open);
+  };
+
   return (
-    <ul className="nav-tabs">
-      {/* <li className="nav-item">
-        <Link
-          to="/about"
-          className={currentPage === '/about' ? 'nav-link-active' : 'nav-link'}
-        >
-          About
-        </Link>
-      </li> */}
-      <li className="nav-item">
-        <Link
-          to="/portfolio"
-          className={currentPage === '/portfolio' ? 'nav-link-active' : 'nav-link'}
-        >
-          Portfolio
-        </Link>
-      </li>
-      {/* <li className="nav-item">
-        <Link
-          to="/contact"
-          className={currentPage === '/contact' ? 'nav-link-active' : 'nav-link'}
-        >
-          Contact
-        </Link>
-      </li> */}
-      <li className="nav-item">
-        <Link
-          to="/resume"
-          className={currentPage === '/resume' ? 'nav-link-active' : 'nav-link'}
-        >
-          Resume
-        </Link>
-      </li>
-    </ul>
+    <>
+      <li className='nav-menu-trigger' onClick={toggleMenu}>X</li>
+      <ul className={`nav-tabs ${isOpen ? 'is-open' : ''}`}>
+        <li className="nav-item">
+          <Link
+            to="/portfolio"
+            className={currentPage === '/portfolio' ? 'nav-link-active' : 'nav-link'}
+          >
+            Portfolio
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/resume"
+            className={currentPage === '/resume' ? 'nav-link-active' : 'nav-link'}
+          >
+            Resume
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 }
 
