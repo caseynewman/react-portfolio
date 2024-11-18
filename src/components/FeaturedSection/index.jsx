49c8eed5fd63d8ed7398c3cd3ef69324a4661c87
Yogi Spice <caseynnewman@yahoo.com>
2024-11-18 13:03:41 -0500

    updated portfolio button to use useLocation

--- src/components/FeaturedSection/index.jsx
+++ src/components/FeaturedSection/index.jsx
@@ -1,9 +1,12 @@
 import './style.css';
 import MyButton from '../Button';
+import { Link, useLocation } from 'react-router-dom';
 import { Image } from '@chakra-ui/react';
 import { useInView } from 'react-intersection-observer';
 
 export default function FeaturedWork() {
+    const currentPage = useLocation().pathname;
+
     const { ref: headingRef, inView: headingIsVisible } = useInView({
         triggerOnce: true,
         rootMargin: '-100px 0px',
@@ -21,15 +24,17 @@ export default function FeaturedWork() {
             <article className='featured-work'>
                 <div className='image-container'>
                     <Image src='/assets/bubble-app.png'
-                        alt='Bubble App'/>
+                        alt='Bubble App' />
                     <Image src='/assets/quiet-soul-homepage.png'
-                        alt='Quiet Soul App'/>
+                        alt='Quiet Soul App' />
                     <Image src='/assets/weather-dashboard-screenshot.png'
-                        alt='Weather Dashboard'/>
+                        alt='Weather Dashboard' />
                 </div>
                 <div className='portfolio-btn-container'>
                     <MyButton>
-                        <a href='/portfolio' target='_blank'>View Portfolio</a>
+                        <Link to="/portfolio">
+                            View Portfolio
+                        </Link>
                     </MyButton>
                 </div>
             </article>
