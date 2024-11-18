e008f33e7b6ab35b852f136abb33910008519d11
Yogi Spice <caseynnewman@yahoo.com>
2024-11-18 14:36:47 -0500

    fixed footer links to open in new page

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,10 +8,10 @@ export default function Footer() {
                 <Link to="/">
                     <img className="footer-logo" src="/assets/cn-header-logo.svg" alt="Casey Newman" />
                 </Link>
-                <Link className="icon" to="https://github.com/caseynewman">
+                <Link className="icon" to="https://github.com/caseynewman" target='_blank'>
                     <img src="/assets/icons/github-mark.svg" alt="GitHub Profile" />
                 </Link>
-                <Link className="icon" to="https://www.linkedin.com/in/caseynewman/">
+                <Link className="icon" to="https://www.linkedin.com/in/caseynewman/" target='_blank'>
                     <img src="/assets/icons/linkedin-icon.svg" alt="LinkedIn Profile" />
                 </Link>
             </div>
